42981e12f47639da37adfb5520770ccb18fe7fe1
El3517 <enejecaleb@gmail.com>
2025-04-20 05:52:10 +0100

    Oops forgot to make navbar sticky, that is done now

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const {setShowSearch, getCartCount} = useContext(ShopContext);
 
     return (
-        <div className='flex items-center justify-between py-5 font-medium'>
+        <div className='sticky top-0 bg-white flex items-center justify-between py-5 font-medium'>
            <Link to='/'><img src={assets.logo} alt="dripcity logo" className='w-24 rounded-full' /></Link>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
